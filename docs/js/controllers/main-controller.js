447036b9dc521d7817e8b6cc595aa59e39e6c742
go-0100-it <davewaters@live.ca>
2017-03-30 21:17:36 -0400

    -

--- docs/js/controllers/main-controller.js
+++ docs/js/controllers/main-controller.js
@@ -4,7 +4,7 @@ define(['jquery', 'backbone', 'underscore', 'drawer_menu_view', 'drawer_menu_lis
     function($, backbone, _, DrawerMenuView, DrawerMenuList, DrawerMenuItem) {
         return {
             renderDrawerListView: function() {
-                var items = [new DrawerMenuItem({ name: 'me', address: 'here' }), new DrawerMenuItem({ name: 'me', address: 'here' }), new DrawerMenuItem({ name: 'me', address: 'here' }), new DrawerMenuItem({ name: 'me', address: 'here' }), new DrawerMenuItem({ name: 'me', address: 'here' })];
+                var items = [new DrawerMenuItem({ name: 'My current address', address: 'here' }), new DrawerMenuItem({ name: 'Cabin in the woods', address: 'here' }), new DrawerMenuItem({ name: 'Colorado Springs Home', address: 'here' }), new DrawerMenuItem({ name: 'Sports complex Indiana', address: 'here' }), new DrawerMenuItem({ name: 'Florida Home Resort', address: 'here' })];
                 var list = new DrawerMenuList(items);
 
                 var drawerListView = new DrawerMenuView({ model: list }).render();
