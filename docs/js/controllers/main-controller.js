ddc41278ef4484ebec66d79b9dd3ce44228297f5
go-0100-it <davewaters@live.ca>
2017-04-02 21:58:45 -0400

    -added image to info window

--- docs/js/controllers/main-controller.js
+++ docs/js/controllers/main-controller.js
@@ -9,11 +9,11 @@ define(['jquery', 'backbone', 'underscore', 'knockback', 'knockout', 'drawer_men
             renderDrawerListView: function() {
 
                 // creating an array of new Backbone models for the individual items of the collection.
-                var items = [new DrawerMenuItem({ name: 'My current address', address: 'here', position: { lat: 43.6898244, lng: -79.61650999999999 }, contentString: '<div><h2>My current address</h2><button>CLICK ME</button></div>' }),
-                    new DrawerMenuItem({ name: 'Cabin in the woods', address: 'here', position: { lat: 43.9898244, lng: -79.31650999999999 }, contentString: '<div><h2>Cabin in the woods</h2><button>CLICK ME</button></div>' }),
-                    new DrawerMenuItem({ name: 'Colorado Springs Home', address: 'here', position: { lat: 43.7898244, lng: -79.21650999999999 }, contentString: '<div><h2>Colorado Springs Home</h2><button>CLICK ME</button></div>' }),
-                    new DrawerMenuItem({ name: 'Sports complex Indiana', address: 'here', position: { lat: 43.6398244, lng: -79.91650999999999 }, contentString: '<div><h2>Sports complex Indiana</h2><button>CLICK ME</button></div>' }),
-                    new DrawerMenuItem({ name: 'Florida Home Resort', address: 'here', position: { lat: 43.7398244, lng: -79.81650999999999 }, contentString: '<div><h2>Florida Home Resort</h2><button>CLICK ME</button></div>' })
+                var items = [new DrawerMenuItem({ name: 'My current address', address: 'here', position: { lat: 43.6898244, lng: -79.61650999999999 } }),
+                    new DrawerMenuItem({ name: 'Cabin in the woods', address: 'here', position: { lat: 43.9898244, lng: -79.31650999999999 } }),
+                    new DrawerMenuItem({ name: 'Colorado Springs Home', address: 'here', position: { lat: 43.7898244, lng: -79.21650999999999 } }),
+                    new DrawerMenuItem({ name: 'Sports complex Indiana', address: 'here', position: { lat: 43.6398244, lng: -79.91650999999999 } }),
+                    new DrawerMenuItem({ name: 'Florida Home Resort', address: 'here', position: { lat: 43.7398244, lng: -79.81650999999999 } })
                 ];
 
                 // creating a new Backbone collection and passing it to the DrawerListViewModel to create an observable collection 
@@ -31,19 +31,34 @@ define(['jquery', 'backbone', 'underscore', 'knockback', 'knockout', 'drawer_men
                     center: uluru
                 });
 
+                function toggleBounce(marker) {
+                    if (marker.getAnimation() !== null) {
+                        marker.setAnimation(null);
+                    } else {
+                        marker.setAnimation(google.maps.Animation.BOUNCE);
+                    }
+                }
+
                 var len = items.length;
                 for (var i = 0; i < len; i++) {
-                    var marker = new google.maps.Marker({ position: items[i].toJSON().position, title: items[i].toJSON().name });
+                    var marker = new google.maps.Marker({
+                        position: items[i].toJSON().position,
+                        title: items[i].toJSON().name,
+                        animation: google.maps.Animation.DROP
+                    });
+
                     var infowindow = new google.maps.InfoWindow({
-                        content: items[i].toJSON().contentString
+                        content: '<div><h2>' + items[i].toJSON().name + '</h2><img src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + items[i].toJSON().position.lat + ', ' + items[i].toJSON().position.lng + '&heading=151.78&pitch=-0.76&key=AIzaSyBSpWUS_wBjBq5kXfnbQO19ewpQPdStRDg"><button>CLICK ME</button></div>'
                     });
                     (function(_infowindow, _map, _marker) {
                         _marker.addListener('click', function() {
+                            toggleBounce(_marker);
                             _infowindow.open(_map, _marker);
                         });
+                        setTimeout(function() {
+                            _marker.setMap(_map);
+                        }, i * 300);
                     })(infowindow, map, marker);
-
-                    marker.setMap(map);
                 }
             }
         };
