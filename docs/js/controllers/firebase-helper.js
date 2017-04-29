0af87a3863ca11d337e1fb17d757013d8bc6d2e6
go-0100-it <davewaters@live.ca>
2017-04-29 13:22:31 -0400

    -

--- docs/js/controllers/firebase-helper.js
+++ docs/js/controllers/firebase-helper.js
@@ -2,13 +2,13 @@ define([],
     function() {
         require(['jquery'],
             function($) {
-                firebase.auth().signInAnonymously().catch(function(error) {
-                    alert('There was an during authentication (error: ' + error.code + '). The app is not connected to the database!')
-                        // Handle Errors here.
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    // ...
-                });
+                // firebase.auth().signInAnonymously().catch(function(error) {
+                //     alert('There was an during authentication (error: ' + error.code + '). The app is not connected to the database!')
+                //         // Handle Errors here.
+                //     var errorCode = error.code;
+                //     var errorMessage = error.message;
+                //     // ...
+                // });
 
                 firebase.auth().onAuthStateChanged(function(user) {
                     if (user) {
